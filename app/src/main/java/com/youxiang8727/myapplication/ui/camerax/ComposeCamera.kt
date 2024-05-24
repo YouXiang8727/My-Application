1fcfaf7410c541ed39caa7871a3af65249b304cb
youxiang8727 <youxiang8727@gmail.com>
2024-05-24 11:34:06 +0800

    feat: composable camera view

--- app/src/main/java/com/youxiang8727/myapplication/ui/camerax/ComposeCamera.kt
+++ app/src/main/java/com/youxiang8727/myapplication/ui/camerax/ComposeCamera.kt
@@ -1,12 +1,24 @@
 package com.youxiang8727.myapplication.ui.camerax
 
+import android.util.Size
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.core.resolutionselector.AspectRatioStrategy
+import androidx.camera.core.resolutionselector.ResolutionSelector
+import androidx.camera.core.resolutionselector.ResolutionStrategy
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.compose.ui.window.DialogProperties
+import androidx.core.content.ContextCompat
 
 @Composable
 fun ComposeCamera(
@@ -22,7 +34,51 @@ fun ComposeCamera(
 
 @Composable
 private fun ComposeCameraView() {
-    Text(text = "ComposeCameraView")
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    val executor = ContextCompat.getMainExecutor(context)
+
+    val previewView = remember {
+        PreviewView(context)
+    }
+
+    val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
+
+    val cameraProvider = cameraProviderFuture.get()
+
+    val cameraSelector = CameraSelector.Builder()
+        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+        .build()
+
+    val resolutionSelector = ResolutionSelector.Builder()
+        .setAspectRatioStrategy(AspectRatioStrategy.RATIO_4_3_FALLBACK_AUTO_STRATEGY)
+        .setResolutionStrategy(
+            ResolutionStrategy.HIGHEST_AVAILABLE_STRATEGY
+        )
+        .build()
+
+    val preview = Preview.Builder()
+        .setResolutionSelector(resolutionSelector)
+        .build()
+        .apply {
+            this.setSurfaceProvider(
+                previewView.surfaceProvider
+            )
+        }
+
+    AndroidView(
+        factory = { ctx ->
+            cameraProviderFuture.addListener({
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    lifecycleOwner,
+                    cameraSelector,
+                    preview
+                )
+            }, executor)
+            previewView
+        })
 }
 
 @Composable
@@ -35,20 +91,20 @@ private fun CameraPermissionDialog(
         onDismissRequest = { },
         confirmButton = {
             Button(
-                onClick = { 
+                onClick = {
                     viewModel.startCameraPermissionActivity(context)
                 }
             ) {
                 Text(text = "OK")
             }
-        }, 
+        },
         properties = DialogProperties(
-            dismissOnBackPress = false, 
+            dismissOnBackPress = false,
             dismissOnClickOutside = false
-        ), 
+        ),
         title = {
             Text(text = "Alert")
-        }, 
+        },
         text = {
             Text(text = "Please open camera permission")
         }
